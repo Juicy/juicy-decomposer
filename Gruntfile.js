85e3708659b4e32ef55abb16f8751d1581b5e70c
Tomek Wytrebowicz <tomalecpub@gmail.com>
2016-03-07 01:25:41 +0100

    Shim V1's  .assignedSlot(), handle adding and removing nodes

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,23 +2,37 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         bump: {
-          options: {
-            files: ['package.json', 'bower.json', 'juicy-composer.html'],
-            commit: true,
-            commitMessage: '%VERSION%',
-            commitFiles: ['package.json', 'bower.json', 'juicy-composer.html'],
-            createTag: true,
-            tagName: '%VERSION%',
-            tagMessage: 'Version %VERSION%',
-            push: false,
-            // pushTo: 'origin',
-            globalReplace: false,
-            prereleaseName: false,
-            regExp: false
-          }
+            options: {
+                files: ['package.json', 'bower.json', 'juicy-composer.html'],
+                commit: true,
+                commitMessage: '%VERSION%',
+                commitFiles: ['package.json', 'bower.json', 'juicy-composer.html'],
+                createTag: true,
+                tagName: '%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                // pushTo: 'origin',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        },
+        watch: {
+            all: {
+                options: {
+                    livereload: true
+                },
+                files: [
+                    '*.html',
+                    'examples/**/*.html',
+                    'test/*.js',
+                    'test/*.html',
+                    'test/**/*.html'
+                ]
+            }
         }
     });
-;
     grunt.loadNpmTasks('grunt-bump');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
 };
